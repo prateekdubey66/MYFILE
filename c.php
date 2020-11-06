eb953f9ba61d3b096e4becfaafeeaa07c410fae5
prateek dubey <prateek.dubey_cs18@gla.ac.in>
2020-11-06 23:33:08 +0530

    first commit

---
+++ c.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    
+    table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+</style>
+<body>
+    <div>
+        <table>
+            <tr>
+                <th>product name</th>
+                <th>product id</th>
+                <th>product quantity</th>
+                <th> product price</th>
+            </tr>    
+            <tr>
+                <td>shivam</td>
+                <td>45</td>
+                <td>5</td>
+                <td>400</td>
+            </tr>    
+            <tr>
+                <td>prateek</td>
+                <td>454</td>
+                <td>54</td>
+                <td>400</td>
+            </tr> 
+            <tr>
+                <td colspan="3"> total</td>
+                <td>800</td>
+            </tr>
+</table>
+</div>
+</body>
+</html>
